fc6b03b256fd08505f4a91b8d2cd9bdaf471a357
llanas <12050570+llanas@users.noreply.github.com>
2025-09-21 19:19:30 +0000

    Deploying to gh-pages from @ llanas-web/llanas@680e7781f79228eb2e20c89874a4b06f7b5136b0 🚀

---
+++ _nuxt/DInRbWik.js
@@ -0,0 +1,1 @@
+import{r as n}from"./s0KKEgGA.js";import a from"./1B03wYzv.js";import{y as s,H as t,h as e,w as i,E as p}from"./BsekNkpZ.js";import"./BcoM21Dg.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
